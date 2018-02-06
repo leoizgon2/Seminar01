012fa672abfdbcaaeabea15e8964f969d5864c16
Leonardo Izquierdo Gonzalez <36045802+leoizgon2@users.noreply.github.com>
2018-02-06 16:57:24 +0100

    Primeros proyectos en android

--- app/src/main/java/com/example/leoizgon/seminar01/MainActivity.java
+++ app/src/main/java/com/example/leoizgon/seminar01/MainActivity.java
@@ -3,8 +3,50 @@ package com.example.leoizgon.seminar01;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"Activity Restart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"Activity Start");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"Activity Resume");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"Activity Pause");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"Activity Stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"Activity Destroyed");
+    }
+
+
+    private static final String TAG="LIFECYCLE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         TextView tv2=(TextView)findViewById(R.id.dynamic_string);
         tv.setText("Dynamic text assigned  by code(int)");
         tv2.setText("Dynamic text assigned by code (String)");
+       Log.d(TAG,"Creating the activity");
 
     }
 }
