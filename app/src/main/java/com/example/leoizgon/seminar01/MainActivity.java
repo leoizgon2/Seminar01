f8c68d1c02f48e94d1d50cbc47c990038524ce26
Leonardo Izquierdo Gonzalez <36045802+leoizgon2@users.noreply.github.com>
2018-02-06 16:00:45 +0100

    Primeros proyectos en android

--- app/src/main/java/com/example/leoizgon/seminar01/MainActivity.java
+++ app/src/main/java/com/example/leoizgon/seminar01/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.leoizgon.seminar01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +11,10 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv=(TextView)findViewById(R.id.tvDynamicTextInt);
-        TextView tv2=(TextView)findViewById(R.id.tvDynamicTextInt);
+        TextView tv=(TextView)findViewById(R.id.dynamic_int);
+        TextView tv2=(TextView)findViewById(R.id.dynamic_string);
+        tv.setText("Dynamic text assigned  by code(int)");
+        tv2.setText("Dynamic text assigned by code (String)");
 
     }
 }
